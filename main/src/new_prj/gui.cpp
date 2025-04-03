ee6f5c3e50f236a466b49d97a56cf2838317a15d
Barry Allen <babanov-artem@yandex.ru>
2025-04-03 21:56:39 +0600

    add delimiters

--- main/src/new_prj/gui.cpp
+++ main/src/new_prj/gui.cpp
@@ -47,33 +47,32 @@ enum class GUI_RoomParameter {
     CO2
 };
 
-// void gui_draw_room_parameter(GUI_RoomParameter paramType, Position pos){
+lv_obj_t* gui_widget_room_parameter_delimiter(lv_obj_t* parent){
+/*Create an array for the points of the line*/
+    static lv_point_precise_t line_points[] = { {0, 5}, {0, 25}};
 
-//     LV_IMG_DECLARE(co2_icon);
-//     lv_obj_t * img1 = lv_img_create(lv_screen_active());
-//     lv_img_set_src(img1, &co2_icon);
-//     lv_obj_set_style_transform_scale(img1, 175, 0);
-//     lv_obj_set_pos(img1, pos.x, pos.y);
+    /*Create style*/
+    static lv_style_t style_line;
+    lv_style_init(&style_line);
+    lv_style_set_line_width(&style_line, 2);
+    lv_style_set_line_color(&style_line, lv_color_hex(0x4D4C4C));
+    lv_style_set_line_rounded(&style_line, true);
 
-//     static lv_style_t style;
-//     lv_style_init(&style);
+    /*Create a line and apply the new style*/
+    lv_obj_t * line1;
+    line1 = lv_line_create(parent);
+    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
+    lv_obj_add_style(line1, &style_line, 0);
+    lv_obj_set_size(line1, 2,30);
+    return line1;
+}
 
-//     uint16_t spacing = 10;
-
-//     lv_obj_t * label_value = lv_label_create(lv_screen_active());
-//     lv_label_set_text(label_value, "333");
-//     lv_obj_set_pos(label_value, pos.x + co2_icon.header.w + spacing, pos.y);
-
-//     lv_style_set_text_font(&style,&lv_font_montserrat_20);
-//     lv_obj_add_style(label_value, &style, LV_STATE_DEFAULT);
-// }
-
-lv_obj_t* gui_draw_room_parameter(lv_obj_t* parent, GUI_RoomParameter paramType, const char* text) {
+lv_obj_t* gui_widget_room_parameter(lv_obj_t* parent, GUI_RoomParameter paramType, const char* text) {
     // Create the main container (104x30)
     lv_obj_t* container = lv_obj_create(parent);
     lv_obj_set_style_bg_opa(container, LV_OPA_TRANSP, 0);  // Make background transparent
     lv_obj_remove_style(container, NULL, LV_PART_MAIN);
-    lv_obj_set_size(container, 94, 30);
+    lv_obj_set_size(container, 90, 30);
     lv_obj_set_flex_flow(container, LV_FLEX_FLOW_ROW);
     lv_obj_set_flex_align(container, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
     lv_obj_set_style_pad_all(container, 0, 0);
@@ -162,6 +161,7 @@ int gui_init(){
     font_normal    = &lv_font_montserrat_12;
 
     lv_obj_set_style_text_font(lv_screen_active(), font_large, 0);
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x0), 0);
 
     lv_obj_t * label1 = lv_label_create(lv_screen_active());
     lv_label_set_text(label1, "12:34");
@@ -179,7 +179,9 @@ int gui_init(){
     // lv_obj_set_style_pad_column(room_parameters, 0, 0);
     lv_obj_clear_flag(room_parameters, LV_OBJ_FLAG_SCROLLABLE);
 
-    gui_draw_room_parameter(room_parameters, GUI_RoomParameter::TEMP, "+30");
-    gui_draw_room_parameter(room_parameters, GUI_RoomParameter::HUMIDITY, "99");
-    gui_draw_room_parameter(room_parameters, GUI_RoomParameter::CO2, "1234");
+    gui_widget_room_parameter(room_parameters, GUI_RoomParameter::TEMP, "+30");
+    gui_widget_room_parameter_delimiter(room_parameters);
+    gui_widget_room_parameter(room_parameters, GUI_RoomParameter::HUMIDITY, "99");
+    gui_widget_room_parameter_delimiter(room_parameters);
+    gui_widget_room_parameter(room_parameters, GUI_RoomParameter::CO2, "1234");
 }
